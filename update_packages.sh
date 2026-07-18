2ad0ff570cf204929241876e645a4c4ee953a6fd
Plutonium Dunn <291398076+pludunn@users.noreply.github.com>
2026-07-17 20:47:21 -0400

    Fix package update script

--- update_packages.sh
+++ update_packages.sh
@@ -1,6 +1,19 @@
-git clone https://github.com/emareg/classlesscss.git /tmp/cssrepo
-cp /tmp/cssrepo/docs/classless.css .
-cp -r /tmp/cssrepo/docs/addons .
-rm -rf /tmp/cssrepo
-curl -LO https://unpkg.com/js-cookie/dist/js.cookie.min.js
-curl -LO https://unpkg.com/@highlightjs/cdn-assets/highlight.min.js
+#!/usr/bin/env bash
+set -euo pipefail
+
+CLASSLESSCSS_REPO="https://github.com/emareg/classlesscss.git"
+JS_COOKIE_URL="https://unpkg.com/js-cookie/dist/js.cookie.min.js"
+HIGHLIGHTJS_URL="https://unpkg.com/@highlightjs/cdn-assets/highlight.min.js"
+
+tmpdir="$(mktemp -d)"
+trap 'rm -rf "$tmpdir"' EXIT
+
+repo_dir="$tmpdir/classlesscss"
+
+git clone --depth 1 "$CLASSLESSCSS_REPO" "$repo_dir"
+cp "$repo_dir/docs/classless.css" .
+rm -rf addons
+cp -R "$repo_dir/docs/addons" .
+
+curl --fail --location --remote-name "$JS_COOKIE_URL"
+curl --fail --location --remote-name "$HIGHLIGHTJS_URL"
